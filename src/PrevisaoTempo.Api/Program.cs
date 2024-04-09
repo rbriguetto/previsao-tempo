6cc92d22a79e2fe858fa33dee6ea5fa8ad23df6e
Rodrigo Brunelli Riguetto <rbriguetto@gmail.com>
2024-04-09 19:52:02 -0300

    feat(api): Criação do projeto da Api

---
+++ src/PrevisaoTempo.Api/Program.cs
@@ -0,0 +1,21 @@
+using PrevisaoTempo.Infraestructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddPrevisaoTempoInfraestructure(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
