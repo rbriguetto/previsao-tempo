89afc08b31ed03480cf75f3376c0c217235cde5c
Rodrigo Brunelli Riguetto <rbriguetto@gmail.com>
2024-04-09 19:56:32 -0300

    feat(api): Criação das api's de CRUD

---
+++ src/PrevisaoTempo.Api/Controllers/CidadesController.cs
@@ -0,0 +1,39 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PrevisaoTempo.Application;
+using PrevisaoTempo.Domain;
+
+namespace PrevisaoTempo.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class CidadesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public CidadesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Cidade>> CriaCidade([FromBody] CriarCidadeCommand model, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(model, cancellationToken);
+        return Ok(response);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Cidade>> AlteraCidade([FromBody] AlterarCidadeCommand model, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(model, cancellationToken);
+        return Ok(response);
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult<Cidade>> ExcluiCidade(int id, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(new ExcluirCidadeCommand() { Id = id }, cancellationToken);
+        return Ok(response);
+    }
+}
